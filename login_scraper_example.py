f692a43ef9f75198a0d7aebaac5093b45118ffe4
Tzahi Vidas <tzahi.vidas@gmail.com>
2015-07-28 00:33:27 -0400

    login scraper example

---
+++ login_scraper_example.py
@@ -0,0 +1,38 @@
+
+import requests
+from lxml import html
+
+USERNAME = "<USERNAME>"
+PASSWORD = "<PASSWORD>"
+
+LOGIN_URL = "https://bitbucket.org/account/signin/?next=/"
+URL = "https://bitbucket.org/dashboard/overview"
+
+def main():
+    session_requests = requests.session()
+
+    # Get login csrf token
+    result = session_requests.get(LOGIN_URL)
+    tree = html.fromstring(result.text)
+    authenticity_token = list(set(tree.xpath("//input[@name='csrfmiddlewaretoken']/@value")))[0]
+
+    # Create payload
+    payload = {
+        "username": USERNAME, 
+        "password": PASSWORD, 
+        "csrfmiddlewaretoken": authenticity_token
+    }
+
+    # Perform login
+    result = session_requests.post(LOGIN_URL, data = payload, headers = dict(referer = LOGIN_URL))
+
+    # Scrape url
+    result = session_requests.get(URL, headers = dict(referer = URL))
+    tree = html.fromstring(result.content)
+    bucket_elems = tree.findall(".//span[@class='repo-name']")
+    bucket_names = [bucket_elem.text_content().replace("\n", "").strip() for bucket_elem in bucket_elems]
+
+    print bucket_names
+
+if __name__ == '__main__':
+    main()
